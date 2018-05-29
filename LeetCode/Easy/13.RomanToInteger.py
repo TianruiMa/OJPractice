b039a869820661f8136be80caae5802e59d40a92
Tianrui Ma <catrinamtr@gmail.com>
2018-05-29 18:09:56 -0400

    update

--- LeetCode/Easy/13.RomanToInteger.py
+++ LeetCode/Easy/13.RomanToInteger.py
@@ -4,30 +4,8 @@ class Solution(object):
         :type num: int
         :rtype: str
         """
-
-        levels = [1000, 500, 100, 50, 10, 5, 1]
-        int_dict = {1000: 'M', 500: 'D', 100: 'C', 50: 'L', 10: 'X', 5: 'V', 1: 'I'}
-
-        roman = ""
-        l = 0
-        while l < len(levels):
-            print("value", levels[l])
-            if num == 0:
-                return roman
-            elif num >= levels[l]:
-                print("find perfect")
-                roman += int_dict[levels[l]]
-                num -= levels[l]
-            else:
-                for d in levels[l+1:][::-1]:
-                    print("d",d)
-                    if num >= levels[l] - d:
-                        roman += int_dict[d] + int_dict[levels[l]]
-                        num -= (levels[l] - d)
-                        break
-                l += 1
-
-        return roman
-
-a = 4
-print(Solution().intToRoman(a))
+        A = ['','I','II','III','IV','V','VI','VII','VIII','IX']
+        B = ['','X','XX','XXX','XL','L','LX','LXX','LXXX','XC']
+        C = ['','C','CC','CCC','CD','D','DC','DCC','DCCC','CM']
+        D = ['','M','MM','MMM']
+        return D[data/1000]+C[data%1000/100]+B[data%100/10]+A[data%10]
